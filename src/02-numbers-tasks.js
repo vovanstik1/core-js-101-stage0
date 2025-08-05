6e0b10a4daeab7bc2c021d812e7355ee3f76cd0c
vovanstik1 <160480356+vovanstik1@users.noreply.github.com>
2025-08-05 11:28:22 +0300

    feat: numbers task complite

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -199,8 +199,19 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2) {
+    return true;
+  }
+  if (n % 2 === 0) {
+    return false;
+  }
+  for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -218,8 +229,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) || Number(def);
 }
 
 module.exports = {
